1af9b54bd59e824166e8b57dfc5cb68c85026604
stantoju <toju2031@gmail.com>
2021-03-11 07:31:13 +0100

    Discoverty client

--- src/main/java/com/wayapaychat/temporalwallet/TemporalWalletApplication.java
+++ src/main/java/com/wayapaychat/temporalwallet/TemporalWalletApplication.java
@@ -2,10 +2,12 @@ package com.wayapaychat.temporalwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TemporalWalletApplication {
 
 	public static void main(String[] args) {
