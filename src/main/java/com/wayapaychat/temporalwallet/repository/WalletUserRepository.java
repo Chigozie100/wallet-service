106d1f993fb00af47998d2cddd6b5d955e8f84b7
Emmanuel Njoku <Emmanuel Njoku@DESKTOP-695LLFB>
2021-07-25 22:43:12 +0100

    Account Closure

--- src/main/java/com/wayapaychat/temporalwallet/repository/WalletUserRepository.java
+++ src/main/java/com/wayapaychat/temporalwallet/repository/WalletUserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.wayapaychat.temporalwallet.entity.WalletAccount;
 import com.wayapaychat.temporalwallet.entity.WalletUser;
 
 @Repository
@@ -17,6 +18,8 @@ public interface WalletUserRepository extends JpaRepository<WalletUser, Long> {
 	WalletUser findByUserId(Long userId);
 	
 	WalletUser findByMobileNo(String phone);
+	
+	WalletUser findByAccount(WalletAccount account);
 	
 	@Query(value = "SELECT u FROM WalletUser u " + "WHERE (UPPER(u.emailAddress) = UPPER(:value) OR "
 			+ "u.mobileNo LIKE CONCAT('%', :value)) AND u.del_flg = false")
