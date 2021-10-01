b10aff4f39648d699ef40312d44b0739a4e62919
citymanjoe <einjoku@gmail.com>
2021-10-01 14:08:12 +0100

    Commission-Update

--- src/main/java/com/wayapaychat/temporalwallet/service/impl/TransAccountServiceImpl.java
+++ src/main/java/com/wayapaychat/temporalwallet/service/impl/TransAccountServiceImpl.java
@@ -849,10 +849,10 @@ public class TransAccountServiceImpl implements TransAccountService {
 
 	public ApiResponse<?> ClientCommissionMoney(ClientComTransferDTO transfer) {
 		WalletAccount acctComm = walletAccountRepository.findByAccountNo(transfer.getDebitAccountNumber());
-		if (!acctComm.getGl_code().equals("SB901")) {
+		if (!acctComm.getProduct_code().equals("SB901")) {
 			return new ApiResponse<>(false, ApiResponse.Code.NOT_FOUND, "NOT COMMISSION WALLET", null);
 		}
-		WalletAccount acctDef = walletAccountRepository.findByAccountNo(transfer.getDebitAccountNumber());
+		WalletAccount acctDef = walletAccountRepository.findByAccountNo(transfer.getBenefAccountNumber());
 		if (!acctDef.isWalletDefault()) {
 			return new ApiResponse<>(false, ApiResponse.Code.NOT_FOUND, "NOT DEFAULT WALLET", null);
 		}
