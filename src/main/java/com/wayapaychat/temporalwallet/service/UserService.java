7071dda3690a11e0c00723c186488c0386710deb
citymanjoe <einjoku@gmail.com>
2021-11-18 14:18:37 +0100

    TO-REMOVE-UNUSED-REPO

--- src/main/java/com/wayapaychat/temporalwallet/service/UserService.java
+++ src/main/java/com/wayapaychat/temporalwallet/service/UserService.java
@@ -1,10 +1,12 @@
-package com.wayapaychat.temporalwallet.service;
-
-import com.wayapaychat.temporalwallet.pojo.UserPojo;
-import org.springframework.http.ResponseEntity;
-
-public interface UserService {
-
-    ResponseEntity<?> createUser(UserPojo userRequest);
-
-}
+/*
+ * package com.wayapaychat.temporalwallet.service;
+ * 
+ * import com.wayapaychat.temporalwallet.pojo.UserPojo; import
+ * org.springframework.http.ResponseEntity;
+ * 
+ * public interface UserService {
+ * 
+ * ResponseEntity<?> createUser(UserPojo userRequest);
+ * 
+ * }
+ */
