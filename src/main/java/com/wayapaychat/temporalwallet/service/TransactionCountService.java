9d9fd651f08ef0bc676775359c026a9816668746
agbeterseer <agbe.terseer@gmail.com>
2022-08-22 11:31:39 +0100

    update on min transcation for non-waya 2

--- src/main/java/com/wayapaychat/temporalwallet/service/TransactionCountService.java
+++ src/main/java/com/wayapaychat/temporalwallet/service/TransactionCountService.java
@@ -1,7 +1,14 @@
 package com.wayapaychat.temporalwallet.service;
 
+import com.wayapaychat.temporalwallet.entity.TransactionCount;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface TransactionCountService {
     long getCount(String userId);
+
+    ResponseEntity<?> getAllUserCount();
 
     void makeCount(String userId, String transactionRef);
 }
