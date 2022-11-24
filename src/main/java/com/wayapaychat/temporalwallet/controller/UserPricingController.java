fd20910f764e52297217a17f4945e7fa3acb64af
Oseni Oluwasina <oluwasina.oseni@dynamowit.com>
2022-11-25 00:48:47 +0100

    fix: security

--- src/main/java/com/wayapaychat/temporalwallet/controller/UserPricingController.java
+++ src/main/java/com/wayapaychat/temporalwallet/controller/UserPricingController.java
@@ -1,7 +1,5 @@
 package com.wayapaychat.temporalwallet.controller;
 
-import com.wayapaychat.temporalwallet.dto.BillerManagementResponse;
-import com.wayapaychat.temporalwallet.exception.CustomException;
 import com.wayapaychat.temporalwallet.service.UserPricingService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -11,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 
@@ -36,15 +34,10 @@ public class UserPricingController {
             @ApiImplicitParam(name = "authorization", dataTypeClass = String.class, value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Sync User with Products", notes = "Custom Pricing", tags = { "USER-PRICING" })
     @GetMapping(value = "/sync-user-products/{apiKey}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> syncUsersWithProducts(@PathVariable String apiKey) {
         return userPricingService.syncWalletUser(apiKey);
     }
-
-//    @GetMapping("/sync-billers/{apiKey}")
-//    @Async
-//    public CompletableFuture<ResponseEntity<List<BillerManagementResponse>>> syncBillers(@PathVariable String apiKey) throws CustomException {
-//        return CompletableFuture.completedFuture(userPricingService.syncBillers(apiKey));
-//    }
 
     @ApiImplicitParams({
             @ApiImplicitParam(name = "authorization", dataTypeClass = String.class, value = "token", paramType = "header", required = true) })
@@ -52,6 +45,7 @@ public class UserPricingController {
     @PutMapping(value = "/custom-pricing", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> updateCustomPrice(@RequestParam("userId") Long  userId, @RequestParam("discountAmount") BigDecimal discountAmount, @RequestParam("customAmount") BigDecimal customAmount, @RequestParam("capAmount") BigDecimal capAmount , @RequestParam("product") String product) {
 
         return CompletableFuture.completedFuture(userPricingService.update(userId, discountAmount, customAmount, capAmount, product));
@@ -64,6 +58,7 @@ public class UserPricingController {
     @PutMapping(value = "/custom-pricing-product", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> updateCustomProductPrice(@RequestParam("capAmount") BigDecimal  capAmount, @RequestParam("discountAmount") BigDecimal discountAmount, @RequestParam("customAmount") BigDecimal customAmount, @RequestParam("product") String product) {
 
         return CompletableFuture.completedFuture(userPricingService.updateCustomProduct(capAmount, discountAmount,customAmount, product));
@@ -76,6 +71,7 @@ public class UserPricingController {
     @GetMapping(value = "/user-products", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> getUsersWithProductsList(@RequestParam(defaultValue = "0") int page,
                                                                          @RequestParam(defaultValue = "10") int size) {
         return CompletableFuture.completedFuture(userPricingService.getAllUserPricing(page,size));
@@ -87,6 +83,7 @@ public class UserPricingController {
     @GetMapping(value = "/user-products/{userId}/{product}", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> getUsersWithProductsListByUserId(@PathVariable String userId, @PathVariable String product) {
         return CompletableFuture.completedFuture(userPricingService.getAllUserPricingUserId(userId,product));
     }
@@ -97,6 +94,7 @@ public class UserPricingController {
     @PostMapping(value = "/apply-discount", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> applyDiscountToAll(@RequestParam("discountAmount") BigDecimal discountAmount) {
 
         return CompletableFuture.completedFuture(userPricingService.applyDiscountToAll(discountAmount));
@@ -108,6 +106,7 @@ public class UserPricingController {
     @PostMapping(value = "/apply-cap-price", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> applyCapToAll(@RequestParam("capAmount") BigDecimal capAmount) {
 
         return CompletableFuture.completedFuture(userPricingService.applyCapToAll(capAmount));
@@ -119,6 +118,7 @@ public class UserPricingController {
     @PostMapping(value = "/apply-general-price", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> applyGeneralToAll(@RequestParam("capAmount") BigDecimal capAmount) {
 
         return CompletableFuture.completedFuture(userPricingService.applyGeneralToAll(capAmount));
@@ -131,6 +131,7 @@ public class UserPricingController {
     @DeleteMapping(value = "/delete-all/{apiKey}", produces =
             MediaType.APPLICATION_JSON_VALUE)
     @Async
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public CompletableFuture<ResponseEntity<?>> deleteUserPricing(@PathVariable String apiKey) {
         return CompletableFuture.completedFuture(userPricingService.deleteAll(apiKey));
     }
--- src/main/java/com/wayapaychat/temporalwallet/controller/WalletBankController.java
+++ src/main/java/com/wayapaychat/temporalwallet/controller/WalletBankController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +36,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
 	@ApiOperation(value = "Create a Wallet Default Special Code", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/code")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> creteDefaultCode(@Valid @RequestBody WalletConfigDTO configPojo) {
         return configService.createDefaultCode(configPojo);
     }
@@ -43,6 +45,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Get List of Wallet Default Special Code", tags = { "BANK-WALLET" })
     @GetMapping(path = "/codes")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getDefaultCode() {
         return configService.getListDefaultCode();
     }
@@ -51,6 +54,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Get Wallet CodeValues using codeValueId", tags = { "BANK-WALLET" })
     @GetMapping(path = "/codeValue/{codeValueId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getCodeValue(@PathVariable("codeValueId") Long codeValueId) {
         return configService.getListCodeValue(codeValueId);
     }
@@ -59,30 +63,35 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Get Wallet CodeValues using codename", tags = { "BANK-WALLET" })
     @GetMapping(path = "/codeValue/{codeName}/command")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> FetchCodeValue(@PathVariable("codeName") String codeName) {
         return configService.getAllCodeValue(codeName);
     }
     
     @ApiOperation(value = "Get Wallet CodeValues using codeId", tags = { "BANK-WALLET" })
     @GetMapping(path = "/codes/{codeId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getCode(@PathVariable("codeId") Long codeId) {
         return configService.getCode(codeId);
     }
     
     @ApiOperation(value = "Create a Wallet Product Code", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/product")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> creteProductCode(@Valid @RequestBody ProductCodeDTO product) {
         return configService.createProduct(product);
     }
     
     @ApiOperation(value = "Get Wallet Product Code", tags = { "BANK-WALLET" })
     @GetMapping(path = "/product/{productId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getProduct(@PathVariable("productId") Long productId) {
         return configService.findProduct(productId);
     }
     
     @ApiOperation(value = "Get Wallet Product Code", tags = { "BANK-WALLET" })
     @GetMapping(path = "/product/code/{productCode}/{glcode}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getProduct(@PathVariable("productCode") String productCode,
     		@PathVariable("glcode") String gl) {
         return configService.getProduct(productCode,gl);
@@ -90,42 +99,49 @@ public class WalletBankController {
     
     @ApiOperation(value = "List Wallet Product Code", tags = { "BANK-WALLET" })
     @GetMapping(path = "/product")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getListProductCode() {
         return configService.ListProductCode();
     }
     
     @ApiOperation(value = "List Account Products", tags = { "BANK-WALLET" })
     @GetMapping(path = "/product/account")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> ListProductAccount() {
         return configService.ListAccountProductCode();
     }
     
     @ApiOperation(value = "Create a Wallet Product Code", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/product/parameter")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createProductParameter(@Valid @RequestBody ProductDTO product) {
         return configService.createProductParameter(product);
     }
     
     @ApiOperation(value = "Create a Wallet Interest Slab", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/interest/parameter")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createInterestParameter(@Valid @RequestBody InterestDTO interest) {
         return configService.createInterestParameter(interest);
     }
     
     @ApiOperation(value = "Create a Wallet Account Chart", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/gl/coa")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createCOA(@Valid @RequestBody AccountGLDTO chat) {
         return configService.createParamCOA(chat);
     }
     
     @ApiOperation(value = "Create a Wallet Teller", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/teller/till")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createTeller(@Valid @RequestBody WalletTellerDTO tellerPojo) {
         return configService.createdTeller(tellerPojo);
     }
     
     @ApiOperation(value = "List Wallet Product Code", tags = { "BANK-WALLET" })
     @GetMapping(path = "/teller/till")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getListTellersTill() {
         return configService.ListTellersTill();
     }
@@ -134,6 +150,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Create a Wallet Event", tags = { "BANK-WALLET" })
     @PostMapping(path = "/create/event")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createEventCharge(@Valid @RequestBody EventChargeDTO eventPojo) {
         return configService.createdEvents(eventPojo);
     }
@@ -142,6 +159,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Update a Wallet Event", tags = { "BANK-WALLET" })
     @PutMapping(path = "/update/event/{eventId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> updateEventCharge(@Valid @RequestBody UpdateEventChargeDTO eventPojo, @PathVariable("eventId") Long eventId) {
         return configService.updateEvents(eventPojo,eventId);
     }
@@ -150,6 +168,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Update a Wallet Event", tags = { "BANK-WALLET" })
     @DeleteMapping(path = "/update/event/{eventId}/delete")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> deleteEventCharge(@PathVariable("eventId") Long eventId) {
         return configService.deleteEvent(eventId);
     }
@@ -158,6 +177,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "List Wallet Event", tags = { "BANK-WALLET" })
     @GetMapping(path = "/event/charges")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getListEventChrg() {
         return configService.ListEvents();
     }
@@ -168,6 +188,7 @@ public class WalletBankController {
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "Get Single Wallet Event", tags = { "BANK-WALLET" })
     @GetMapping(path = "/event/charges/{chargeId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_INITIATOR', 'ROLE_ADMIN_APPROVAL', 'ROLE_ADMIN_REPORT', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> getSingleEventCharge(@PathVariable("chargeId") Long chargeId) {
         return configService.getSingleEvents(chargeId);
     }
@@ -178,23 +199,16 @@ public class WalletBankController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "authorization", dataTypeClass = String.class, value = "token", paramType = "header", required = true) })
     @PostMapping(path = "/create/transaction/charge")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> createTransactionCharge(@Valid @RequestBody ChargeDTO charge) {
         return configService.createCharge(charge);
     }
 
-
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
-//    @ApiOperation(value = "List Transaction Charge", tags = { "BANK-WALLET" })
-//    @GetMapping(path = "/transaction/charges")
-//    public ResponseEntity<?> ListAllCharge() {
-//        return configService.ListTranCharge();
-//    }
-//
     @ApiImplicitParams({
             @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
     @ApiOperation(value = "AUTO CREATE ACCOUNT", tags = { "BANK-WALLET" })
     @PostMapping(path = "/auto-create/account")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN_OWNER', 'ROLE_ADMIN_SUPER', 'ROLE_ADMIN_APP')")
     public ResponseEntity<?> AutoCreateTransAccount(@RequestBody AutoCreateAccount request) {
         ResponseEntity<?> responseEntity = configService.AutoCreateTransAccount(request);
         if (responseEntity.getStatusCode().is2xxSuccessful()){
@@ -216,17 +230,5 @@ public class WalletBankController {
         }
         return responseEntity;
     }
-//
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "authorization", value = "token", paramType = "header", required = true) })
-//    @ApiOperation(value = "Update Transaction Charge", tags = { "BANK-WALLET" })
-//    @PutMapping(path = "/transaction/charges/{chargeId}")
-//    public ResponseEntity<?> creteDefaultCode(@Valid @RequestBody ModifyChargeDTO charge,
-//    		@PathVariable("chargeId") Long chargeId) {
-//        return configService.updateTranCharge(charge,chargeId);
-//    }
-
-
-
 
 }
