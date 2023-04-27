cce6163eeee8ead75b5b5a2ce6a15a7aca0776bb
oluwatosin247 <oluwatosin.fasian@wayapaychat.com>
2023-04-27 12:21:53 +0100

    commit

--- src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransAccountRepository.java
+++ src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransAccountRepository.java
@@ -10,50 +10,59 @@ import org.springframework.stereotype.Repository;
 public interface WalletTransAccountRepository extends JpaRepository<WalletTransAccount, Long> {
 
     @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'QUICKTELLER' "
-            + " OR u.eventId = 'BAXI' ")
+            + " OR u.eventId = 'BAXI' AND u.status = 'SUCCESSFUL'")
     BigDecimal findByAllBillsTransaction();
 
-    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'NIP_PAYOUT' ")
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'NIP_PAYOUT' AND "
+            + "u.status = 'SUCCESSFUL'")
     BigDecimal findByAllOutboundExternalTransaction();
 
-    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'WAYATRAN' ")
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'WAYATRAN' AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findByAllOutboundInternalTransaction();
 
-    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'NIP_FUNDING' ")
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'NIP_FUNDING' AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findByAllInboundTransaction();
 
-    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u ")
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.status = 'SUCCESSFUL' ")
     BigDecimal totalTransactionAmount();
 
-    @Query("SELECT sum(u.chargeAmount) FROM WalletTransAccount u ")
+    @Query("SELECT sum(u.chargeAmount) FROM WalletTransAccount u WHERE u.status = 'SUCCESSFUL'")
     BigDecimal totalRevenueAmount();
 
-    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'PAYSTACK_FUNDING' ")
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'PAYSTACK_FUNDING' AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findByAllPaystackTransaction();
 
     @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.creditAccountNumber = (:accountNumber) "
-            + " OR u.debitAccountNumber = (:accountNumber)")
+            + " OR u.debitAccountNumber = (:accountNumber) AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findByAllTransactionByUser(String accountNumber);
 
     @Query("SELECT count(u.id) FROM WalletTransAccount u WHERE u.eventId = 'QUICKTELLER' "
-            + " OR u.eventId = 'BAXI' ")
+            + " OR u.eventId = 'BAXI' AND "
+            + "u.status = 'SUCCESSFUL' ")
     long countBillsTransaction();
 
     @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.debitAccountNumber = (:accountNumber) "
             + " AND u.eventId = 'QUICKTELLER' "
-            + " OR u.eventId = 'BAXI' ")
+            + " OR u.eventId = 'BAXI' AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findBillsTransactionByUser(String accountNumber);
-    
-     @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.debitAccountNumber = (:accountNumber) "
+
+    @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.debitAccountNumber = (:accountNumber) "
             + " AND u.eventId = 'QUICKTELLER' "
-            + " OR u.eventId = 'BAXI' ")
+            + " OR u.eventId = 'BAXI' AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findNipInboundByUser(String accountNumber);
-    
-    
-    @Query("SELECT sum(u.chargeAmount) FROM WalletTransAccount u WHERE u.debitAccountNumber = (:accountNumber) ")
+
+    @Query("SELECT sum(u.chargeAmount) FROM WalletTransAccount u WHERE u.debitAccountNumber = (:accountNumber) AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal totalRevenueAmountByUser(String accountNumber);
-    
+
     @Query("SELECT sum(u.tranAmount) FROM WalletTransAccount u WHERE u.eventId = 'NIP_FUNDING' "
-            + "AND u.creditAccountNumber = (:accountNumber) ")
+            + "AND u.creditAccountNumber = (:accountNumber) AND "
+            + "u.status = 'SUCCESSFUL' ")
     BigDecimal findByAllInboundTransactionByUser(String accountNumber);
 }
