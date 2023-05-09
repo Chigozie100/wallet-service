514277dbc1490e7ed6bc248216ffcc11334ef9ad
Bolaji Oyewumi <49565233+omabolaji@users.noreply.github.com>
2023-05-09 18:18:59 +0100

    update on kafka dto

--- src/main/java/com/wayapaychat/temporalwallet/pojo/signupKafka/SignUpDto.java
+++ src/main/java/com/wayapaychat/temporalwallet/pojo/signupKafka/SignUpDto.java
@@ -1,5 +1,7 @@
 package com.wayapaychat.temporalwallet.pojo.signupKafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 
 @Data
@@ -7,4 +9,12 @@ public class SignUpDto {
     private UserDataDto user;
     private ProfileDataDto profile;
     private String token;
+
+    public SignUpDto(String jsonString) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        SignUpDto dto = mapper.readValue(jsonString, SignUpDto.class);
+        this.user = dto.user;
+        this.profile = dto.profile;
+        this.token = dto.token;
+    }
 }
