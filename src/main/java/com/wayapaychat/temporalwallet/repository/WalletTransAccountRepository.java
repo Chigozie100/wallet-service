bd71ca08cdbf59bc84998079398cb13c113b85e8
oluwatosin247 <oluwatosin.fasian@wayapaychat.com>
2023-06-13 12:37:31 +0100

    commit

--- src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransAccountRepository.java
+++ src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransAccountRepository.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -107,6 +109,8 @@ public interface WalletTransAccountRepository extends JpaRepository<WalletTransA
     BigDecimal findByAllOutboundInternalTransactionByDate(Date fromtranDate, Date totranDate);
     
   
+   @Query("SELECT u FROM WalletTransAccount u WHERE u.debitAccountNumber =: accountNo OR u.creditAccountNumber =: accountNo " + " order by u.createdAt DESC ")
+    Page<WalletTransAccount> findByAccount(Pageable pageable,  String accountNo);
 
 
 
