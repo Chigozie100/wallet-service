edfca9b7d3deb4e14ee9960e2b7a7eece177c560
Oseni Oluwasina <84130142+oluwasinaoseni@users.noreply.github.com>
2023-06-16 10:48:35 +0100

    fix: db session using transaction

--- src/main/java/com/wayapaychat/temporalwallet/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/wayapaychat/temporalwallet/service/impl/UserAccountServiceImpl.java
@@ -35,6 +35,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.security.SecureRandom;
@@ -2050,6 +2052,7 @@ public class UserAccountServiceImpl implements UserAccountService {
 
     }
 
+    @Transactional
     @Override
     public ResponseEntity<?> setupAccountOnExternalCBA(String accounNumber) {
 
