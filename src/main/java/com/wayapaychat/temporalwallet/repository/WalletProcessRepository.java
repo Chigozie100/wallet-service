c72472a699a695d6cc9658c987cb7c57b7850419
Oseni Oluwasina <84130142+oluwasinaoseni@users.noreply.github.com>
2023-07-18 15:54:39 +0100

    fix: query error

--- src/main/java/com/wayapaychat/temporalwallet/repository/WalletProcessRepository.java
+++ src/main/java/com/wayapaychat/temporalwallet/repository/WalletProcessRepository.java
@@ -9,8 +9,8 @@ import com.wayapaychat.temporalwallet.entity.WalletProcess;
 @Repository
 public interface WalletProcessRepository extends JpaRepository<WalletProcess, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT TOP 1 u FROM WalletProcess u "
-            + "WHERE UPPER(u.processName) = UPPER(:processName) " + " ORDER BY id DESC ")
+    @Query( "SELECT u FROM WalletProcess u "
+            + "WHERE UPPER(u.processName) = UPPER(:processName) " + " ORDER BY id DESC LIMIT 1 ")
     WalletProcess findLastProcessExecuted(String processName);
 
 }
--- src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransactionRepository.java
+++ src/main/java/com/wayapaychat/temporalwallet/repository/WalletTransactionRepository.java
@@ -22,8 +22,7 @@ public interface WalletTransactionRepository extends JpaRepository<WalletTransac
 
     List<WalletTransaction> findByAcctNumEquals(String accountNumber);
 
-    @Query(nativeQuery = true,
-            value = "SELECT TOP 1 u FROM WalletTransaction u " + "WHERE u.acctNum NOT LIKE 'NGN%' " + " AND u.del_flg = false ORDER BY id ASC ")
+    @Query( "SELECT u FROM WalletTransaction u " + "WHERE u.acctNum NOT LIKE 'NGN%' " + " AND u.del_flg = false ORDER BY id ASC LIMIT 1")
     WalletTransaction findFirstCustomerTransaction();
 
     @Query("SELECT u FROM WalletTransaction u " + "WHERE UPPER(u.tranId) = UPPER(:value) " + " AND u.del_flg = false")
