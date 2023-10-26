5b2ea1fa6a4f3f39994e57f60ec208f2be6bd2bd
Oseni Oluwasina <84130142+oluwasinaoseni@users.noreply.github.com>
2023-10-26 10:26:59 +0100

    fix: get request ip

--- src/main/java/com/wayapaychat/temporalwallet/controller/WalletTransactionController.java
+++ src/main/java/com/wayapaychat/temporalwallet/controller/WalletTransactionController.java
@@ -764,7 +764,12 @@ public class WalletTransactionController {
         @ApiImplicitParam(name = "authorization", dataTypeClass = String.class, value = "token", paramType = "header", required = true)})
     @ApiOperation(value = "Total Overall and category Based Transactions", notes = "Total Overall and category Based Transactions", tags = {"TRANSACTION-WALLET"})
     @GetMapping("/transaction/overall-based-analysis")
-    public ResponseEntity<?> overallBasedAnalysis() {
+    public ResponseEntity<?> overallBasedAnalysis(HttpServletRequest request) {
+        final String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader != null) {
+            log.info(xfHeader.split(",")[0]);
+        }
+        log.info(request.getRemoteAddr());
         return transAccountService.transactionAnalysis();
     }
 
