9f490e92443def1152a11c7d982e8d00bbf46a06
Oseni Oluwasina <84130142+oluwasinaoseni@users.noreply.github.com>
2024-05-24 09:51:00 +0100

    fix: deployment

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,4 +7,4 @@ sed -i "s/cfgprofile/$NAMESPACE/" deploy.yaml
 
 aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $CLUSTER_NAME
 
-kubectl apply -f deploy.yaml --namespace=$NAMESPACE
+kubectl apply -f deploy.yaml --namespace=wayabank-$NAMESPACE
